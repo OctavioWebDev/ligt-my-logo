032bbe88d0b84ac856794830937cee65d092d6ec
Octavio Sanchez <tavowebdev419@gmail.com>
2024-08-22 21:52:42 -0400

    ok

--- src/components/WhyChooseUsSection.tsx
+++ src/components/WhyChooseUsSection.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function WhyChooseUsSection() {
   return (
-    <section className="w-full border-b py-6 md:py-12 lg:py-16 xl:py-18 2xl:py-20">
+    <section className="w-full py-6 md:py-12 lg:py-16 xl:py-18 2xl:py-20">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
